25bda8fddba217a93061f069489fe14d29595e64
gleb032 <fandeev.business@gmail.com>
2024-07-26 16:49:07 +0300

    Initial Commit

---
+++ LiveLookUp/LiveLookUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveLookUpApp.swift
+//  LiveLookUp
+//
+//  Created by Глеб Фандеев on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveLookUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
